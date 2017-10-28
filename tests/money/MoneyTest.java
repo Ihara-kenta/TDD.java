fbbe8f246fecce4d0b3301e80f5a2bb2d30b8828
ihara-kenta <mayonnaise.3048.meter@gmail.com>
2017-10-29 00:21:16 +0900

    [add]equalsメソッドの追加.仮実装 p21

--- tests/money/MoneyTest.java
+++ tests/money/MoneyTest.java
@@ -6,10 +6,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MoneyTest {
     @Test
     public void testMultiplication(){
-        money.Dollar five = new money.Dollar(5);
-        money.Dollar product = five.times(2);
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+    @Test
+    public void testEquality(){
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
     }
 }
