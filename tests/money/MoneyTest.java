77367855f4026814bc78686700ebfa74c0165f4c
ihara-kenta <mayonnaise.3048.meter@gmail.com>
2017-10-29 16:08:17 +0900

    [add]Moneyクラスの追加. equalsメソッドの一般化 p42

--- tests/money/MoneyTest.java
+++ tests/money/MoneyTest.java
@@ -3,7 +3,7 @@ package money;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class MoneyTest {
+class MoneyTest {
     @Test
     public void testMultiplication(){
         Dollar five = new Dollar(5);
@@ -14,6 +14,8 @@ public class MoneyTest {
     public void testEquality(){
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
     }
     @Test
     public void testFrancMultiplication(){
